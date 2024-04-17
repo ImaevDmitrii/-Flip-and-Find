9f5d7bb5d7ce51f87db7be0276c1a85d2cdfe3ce
Dmitrii Imaev <imaev.dima@gmail.com>
2024-04-17 10:19:48 +0300

    add MainView

---
+++ FlipAndFind/FlipAndFind/Coordinator/MainCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  MainCoordinator.swift
+//  FlipAndFind
+//
+//  Created by Dmitrii Imaev on 14.04.2024.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    var navigationController: UINavigationController { get set }
+    func start()
+}
+
+final class MainCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let mainController = MainViewController()
+        navigationController.pushViewController(mainController, animated: true)
+    }
+    
+    func startGame() {
+        let gameViewController = MainViewController()
+        navigationController.pushViewController(gameViewController, animated: true)
+    }
+    
+    func showMyGames() {
+        let resultsViewController = MainViewController()
+        navigationController.pushViewController(resultsViewController, animated: true)
+    }
+    
+    func showSettings() {
+        let settingsViewController = MainViewController()
+        navigationController.pushViewController(settingsViewController, animated: true)
+    }
+    
+}
+
+
